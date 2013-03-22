860c34ae150f1bfa52dc6eb148cf3d76ccc22337
Francois Perron <fperron@gmail.com>
2013-03-22 19:57:11 -0400

    lint it

--- src/creation.js
+++ src/creation.js
@@ -4,19 +4,19 @@ function Creation() {
 
 }
 
-Creation.prototype.setMessageTemplate = function(template) {
-	this.template = template;
-}
+Creation.prototype.setMessageTemplate = function (template) {
+    this.template = template;
+};
 
-Creation.prototype.getMessageTemplate = function() {
-	return this.template;
-}
+Creation.prototype.getMessageTemplate = function () {
+    return this.template;
+};
 
-Creation.prototype.execute = function(request, response) {
-	var html = this.template.toString()
-		.replace(messageToken, 
-				 "saved: goal scored by player 23 and assisted by player 10");
-	response.write(html);
+Creation.prototype.execute = function (request, response) {
+    var html = this.template.toString()
+        .replace(messageToken,
+            "saved: goal scored by player 23 and assisted by player 10");
+    response.write(html);
 };
 
 module.exports = Creation; 
