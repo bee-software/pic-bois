c146cb5dbf28f755b6b266b2d806bf612db83d86
ericminio <eric.mignot@gmail.com>
2013-03-22 12:31:17 -0400

    … it is :)

--- src/servingRoute.js
+++ src/servingRoute.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 servingRoute = function(folder) {
 	
 	return function (request, response) {
+		response.setHeader("content-type", "text/html");
 		if (request.url == "/goals/create") {
 			var html = "<label id=message>" 
 			+ "saved: goal scored by player 23 and assisted by player 10"
