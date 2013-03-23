d86ca3fe48c2a9284224339ca73f239cc73c820f
Francois Perron <fperron@gmail.com>
2013-03-23 19:28:21 -0400

    added a feature tests

--- features/submit.goal.spec.js
+++ features/submit.goal.spec.js
@@ -24,8 +24,19 @@ describe("submit goal", function() {
 				done();
 			});
 		});
-	});	
-	
+	});
+
+    it("displays the players numbers in the saved message", function(done) {
+   		browser.visit("http://localhost:5000/goals/new", function () {
+   			browser.fill("#scoredBy", "99");
+   			browser.fill("#assistedBy", "66");
+   			browser.pressButton("#submit", function() {
+   				expect(browser.text("#message")).toEqual("saved: goal scored by player 99 and assisted by player 66");
+   				done();
+   			});
+   		});
+   	});
+
 });
-		
+
 		
