802de78259972193b615f268f6129a8e14bf5362
Francois Perron <fperron@gmail.com>
2013-03-27 10:17:27 -0400

    add sef

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
-function Server(serving) {
-	this.serving = serving;
-}
+(function() {
+    "use strict";
 
-Server.prototype.start = function() {
-	this.server = require("http").createServer(this.serving).listen(process.env.PORT || 5000);
-};
+    function Server(serving) {
+        this.serving = serving;
+    }
 
-Server.prototype.stop = function() {
-	this.server.close();
-};
+    Server.prototype.start = function() {
+        this.server = require("http").createServer(this.serving).listen(process.env.PORT || 5000);
+    };
 
-module.exports = Server;
+    Server.prototype.stop = function() {
+        this.server.close();
+    };
+
+    module.exports = Server;
+}());
