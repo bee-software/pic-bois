5fd8c06c216f1534462805a5faf81828ab412bfc
Francois Perron <fperron@gmail.com>
2013-03-31 11:28:54 -0400

    move require() at the top of file to clearly identified dependencies

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 (function() {
     "use strict";
 
+    var http = require("http");
+
     function Server(serving) {
         this.serving = serving;
     }
 
     Server.prototype.start = function() {
-        this.server = require("http").createServer(this.serving).listen(process.env.PORT || 5000);
+        this.server = http.createServer(this.serving).listen(process.env.PORT || 5000);
     };
 
     Server.prototype.stop = function() {
