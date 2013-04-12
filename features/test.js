ab2a203ffe6fded424464eab12327bd3ea9540e4
Francois Perron <fperron@gmail.com>
2013-04-12 17:01:17 -0400

    install karma

---
+++ features/test.js
@@ -0,0 +1,11 @@
+(function () {
+    "use strict";
+
+    describe("test karma installation", function () {
+
+        it("runs", function () {
+            expect("hello").toEqual("hello");
+        });
+    });
+
+}());
