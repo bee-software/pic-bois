33f6161c56f45fc3044f3d7bf5da7179e90e7d18
Carl Letourneau <cletourneau@gmail.com>
2013-04-17 11:30:47 -0400

    Removed useless utils.py phantomjs path is all in features/__init__.py

--- features/__init__.py
+++ features/__init__.py
@@ -1,5 +1,13 @@
 import inspect
-from picbois.utils import get_current_file_dir
+import os
+
+
+def get_current_file_path(currentframe):
+    return os.path.abspath(inspect.getfile(currentframe))
+
+
+def get_current_file_dir(currentframe):
+    return os.path.dirname(get_current_file_path(currentframe))
 
 
 def phantomjs():
--- picbois/utils.py
+++
@@ -1,8 +0,0 @@
-import inspect
-import os
-
-def get_current_file_path(currentframe):
-    return os.path.abspath(inspect.getfile(currentframe))
-
-def get_current_file_dir(currentframe):
-    return os.path.dirname(get_current_file_path(currentframe))
