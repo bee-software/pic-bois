7f78737d14339af5d9fcb340b9033ff8aa487be1
Francois Perron <fperron@gmail.com>
2013-04-18 16:47:53 -0400

    remove dead code in mark_goal_test

--- features/mark_goal_test.py
+++ features/mark_goal_test.py
@@ -25,19 +25,3 @@ class MarkGoal(unittest.TestCase):
     def tearDownClass(cls):
         cls.server.shutdown()
         cls.browser.quit()
-
-
-
-        #
-        # it("displays the players' numbers in the saved message", function (done) {
-        #     browser.visit("http://localhost:5000/goals/new", function () {
-        #     browser.fill("#scoredBy", "99");
-        # browser.fill("#assistedBy", "66");
-        # browser.pressButton("#submit", function () {
-        #     expect(browser.text("#message")).toEqual("saved: goal scored by player 99 and assisted by player 66");
-        # done();
-        # });
-        # });
-        # });
-        #
-        # });
