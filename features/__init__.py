d4d892beeda3e7e8a2695492f1380b0fc47045aa
Francois Perron <fperron@gmail.com>
2013-04-19 17:34:30 -0400

    reorder functions, but still not happy with them

--- features/__init__.py
+++ features/__init__.py
@@ -2,13 +2,16 @@ import inspect
 import os
 
 
-def get_current_file_path(currentframe):
-    return os.path.abspath(inspect.getfile(currentframe))
+def phantomjs():
+    return get_current_file_dir(inspect.currentframe()) + '/../node_modules/phantomjs/lib/phantom/bin/phantomjs'
 
 
 def get_current_file_dir(currentframe):
     return os.path.dirname(get_current_file_path(currentframe))
 
 
-def phantomjs():
-    return get_current_file_dir(inspect.currentframe()) + '/../node_modules/phantomjs/lib/phantom/bin/phantomjs'
+def get_current_file_path(currentframe):
+    return os.path.abspath(inspect.getfile(currentframe))
+
+
+
