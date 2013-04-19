41bff4cc22aed8b1dccf3b7520ca5e6cf17d6516
Francois Perron <fperron@gmail.com>
2013-04-19 08:22:26 -0400

    show home when no anchored page found

--- picbois/www/static/src/single_page_navigation.js
+++ picbois/www/static/src/single_page_navigation.js
@@ -2,18 +2,23 @@ var dhhr = {};
 
 (function () {
     "use strict";
+    var HOME = 'home.html';
 
-    var SinglePage = dhhr.SinglePage = function(anchoredRoutes) {
-        this.anchoredRoutes = anchoredRoutes;
+    var SinglePage = dhhr.SinglePage = function(routes) {
+        this.routes = routes;
     };
 
     SinglePage.prototype.onLoad = function(){
-        _loadPageInMainArea('home.html');
+        _loadPageInMainArea(HOME);
     };
 
-    SinglePage.prototype.loadAnchoredPage = function(){
-        var page = this.anchoredRoutes[window.location.hash];
+    SinglePage.prototype.loadAnchoredPage = function() {
+        var page = this.getPageFromHash();
         _loadPageInMainArea(page);
+    };
+
+    SinglePage.prototype.getPageFromHash = function() {
+        return this.routes[window.location.hash] || HOME;
     };
 
     function _loadPageInMainArea(page) {
--- picbois/www/static/src/single_page_navigation_test.js
+++ picbois/www/static/src/single_page_navigation_test.js
@@ -31,6 +31,17 @@ describe("Single page navigation", function(){
         expect($('#main').html()).toEqual("Anchored page content");
     });
 
+    it("loads the home page in the main area when there is no page associated with the window.location.hash", function(){
+        spyOn($, "ajax").andCallFake(function (params) {params.success("Home page content");});
+
+        window.location.hash = "#MalformedAnchoredPage";
+        var singlePage = new dhhr.SinglePage({'#AnchoredPage': 'anchoredPage.html'});
+        singlePage.loadAnchoredPage();
+
+        expect($.ajax.mostRecentCall.args[0].url).toBe('home.html');
+        expect($('#main').html()).toEqual("Home page content");
+    });
+
     it("loads previous state in the main area on a browser back event", function(){
 
     });
